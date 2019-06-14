8fac8a5a24499f3d00a7bcac7191af7790b2505e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-06-14 17:30:08 -0400

    Updates

---
+++ precache-manifest.aa2a76b9d5558f0d26531391c3f14b13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f184ad961a1ea9059af8491d480ee32",
+    "url": "/react-wavesurfer-demo/index.html"
+  },
+  {
+    "revision": "68dd4a975ac3ec4bf17a",
+    "url": "/react-wavesurfer-demo/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "2c6f71468a6a471b938c",
+    "url": "/react-wavesurfer-demo/static/js/2.d4bc6250.chunk.js"
+  },
+  {
+    "revision": "68dd4a975ac3ec4bf17a",
+    "url": "/react-wavesurfer-demo/static/js/main.4f3d6d36.chunk.js"
+  },
+  {
+    "revision": "33a1f85cda3e679334d5",
+    "url": "/react-wavesurfer-demo/static/js/runtime~main.606067bc.js"
+  },
+  {
+    "revision": "7d93b7db92f6aa18674523536f79afc8",
+    "url": "/react-wavesurfer-demo/static/media/piano.7d93b7db.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-wavesurfer-demo/precache-manifest.dad203c17827bd52021f82fd5d82fb50.js"
+  "/react-wavesurfer-demo/precache-manifest.aa2a76b9d5558f0d26531391c3f14b13.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-wavesurfer-demo/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-wavesurfer-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
