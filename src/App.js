81295f658010f282a61515cf67aff5faa17ed230
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-10-17 14:18:33 -0400

    add footer links

--- src/App.js
+++ src/App.js
@@ -5,15 +5,43 @@ import pianoClip from './piano.mp3';
 
 const App = () => {
   return (
-    <Wrap>
+    <main style={{ padding: '16px' }}>
       <h1>WaveSurfer Demo</h1>
       <Waveform audio={pianoClip} />
-    </Wrap>
+      <footer style={{ marginTop: '32px' }}>
+        <p>
+          Made by
+          {' '}
+          <Link href="https://andreidobrinski.com">Andrei Dobrinski</Link>
+        </p>
+        <p>Check out the
+          {' '}
+          <Link href="https://andreidobrinski.com/blog/implementing-an-audio-waveform-with-react-hooks-and-wavesurferjs/">tutorial</Link>
+          {' '}
+          and
+          {' '}
+          <Link href="https://github.com/andreidobrinski/react-wavesurfer-demo/blob/master/src/waveform.js">source code.</Link>
+        </p>
+      </footer>
+    </main>
   );
 }
 
-const Wrap = styled.div`
-  padding: 16px;
+const Link = styled.a`
+  --primary: #61dbff;
+  color: rgba(0, 0, 0, 0.9);
+  text-decoration: none;
+  padding: 0 1px;
+  box-shadow: inset 0 -6px 0 0 var(--primary);
+  transition: box-shadow 0.3s ease-in;
+
+  &:hover {
+    box-shadow: inset 0 -40px 0 0 var(--primary);
+  }
+
+  &:active {
+    box-shadow: inset 0 -40px 0 0 var(--primary);
+  }
 `;
 
 export default App;
